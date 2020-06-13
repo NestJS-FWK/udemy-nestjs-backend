5f763874292a1e41b0ba3dfcb0589e8c9066df37
Fidel Santana <in3pi287@gmail.com>
2020-06-13 13:34:21 -0400

    Persistence: Getting Tasks (with or without filters)

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,10 +2,27 @@ import { Repository, EntityRepository } from "typeorm";
 import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
+import { GetTaskFilterDto } from "./dto/get-tasks-filter.dto";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task>{
-    
+
+    async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
+        const query = this.createQueryBuilder('task');
+
+        if (status) {
+            query.andWhere('task.status = :status', { status });
+        }
+
+        if (search) {
+            query.andWhere('task.title LIKE :search OR task.description LIKE :search', { search: `%${search}%` });
+        }
+
+        const task = await query.getMany();
+        return task;
+    }
+
     async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
         const { title, description } = createTaskDto;
         const task = new Task();
