8b0bacf52379c23713f8aff1699dbf14ccda7dd9
Fidel Santana <in3pi287@gmail.com>
2020-06-13 13:10:08 -0400

    IMPORTANT: Before Persistence of Getting Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,28 +11,11 @@ export class TasksService {
     constructor(
         @InjectRepository(TaskRepository)
         private taskRepository: TaskRepository,
-    ) {}
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
+    ) { }
 
-    // getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let tasks = this.getAllTasks();
+    getTasks(filterDto: GetTaskFilterDto) {
 
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
-
-    //     if (search) {
-    //         tasks = tasks.filter(task =>
-    //             task.title.includes(search) ||
-    //             task.description.includes(search)
-    //         );
-    //     }
-
-    //     return tasks;
-    // }
+    }
 
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepository.findOne(id);
@@ -52,7 +35,7 @@ export class TasksService {
         const result = await this.taskRepository.delete(id);
 
         if (result.affected == 0) {
-            throw new NotFoundException(`Task with ID "${id}" not found`);    
+            throw new NotFoundException(`Task with ID "${id}" not found`);
         }
     }
 
