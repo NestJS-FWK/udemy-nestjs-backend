24a556e152ed72a333598e00b1b8016b4432760c
Fidel Santana <in3pi287@gmail.com>
2020-06-14 12:55:12 -0400

    Feature: Validating Password - Sign In

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,12 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('/signup')
-    signup(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.authService.signIn(authCredentialsDto);
     }
 }
