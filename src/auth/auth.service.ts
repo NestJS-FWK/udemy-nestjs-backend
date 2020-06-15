941e0aa7da3a1144e90c1b5229327698ab869b2a
Fidel Santana <in3pi287@gmail.com>
2020-06-14 20:20:55 -0400

    Applying logging in our application

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { JwtPayload } from './jwt-payload.interface';
+import { json } from 'express';
 
 @Injectable()
 export class AuthService {
+    private logger = new Logger;
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
@@ -26,6 +28,7 @@ export class AuthService {
 
         const payload: JwtPayload = { username };
         const accessToken = await this.jwtService.sign(payload);
+        this.logger.debug(`Generated JWT Token with payload ${JSON.stringify(payload)}`);
 
         return { accessToken };
     }
